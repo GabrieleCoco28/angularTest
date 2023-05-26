bec174a4a1384e0c9e9091677fbc392924155e9e
gabri <gabrielecoco0@mail.com>
2023-05-26 12:31:10 +0200

    finito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +33,8 @@ import { CardComponent } from './card/card.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
+    MatInputModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: BodyComponent },
